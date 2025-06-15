8d2fce0421fc34c9f94b7642f48070693c4eda9b
Siddharth <bharadiasj@gmail.com>
2025-06-15 18:00:51 +0530

    fixed bugs

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,6 @@ app.use("/api/support", supportRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.get("/", ()=>{
+app.get("/", (req, res)=>{
   res.send("Server is running");
 })
